30d1fb0c8f69176641e016bbb7f35e8c4c7289e9
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-04-28 19:42:01 -0300

    build: restart the project foundation

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, VariantTypes } from "./Button.styles";
-
-interface ButtonProps {
-  variant?: VariantTypes;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Click me</ButtonContainer>;
-}
--- src/styles/theme/default.ts
+++
@@ -1,9 +0,0 @@
-export const defaultTheme = {
-  colors: {
-    primary: "#007bff",
-    secondary: "#6c757d",
-    danger: "#dc3545",
-    success: "#28a745",
-    white: "#fff",
-  },
-}
