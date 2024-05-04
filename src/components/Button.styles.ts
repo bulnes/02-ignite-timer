36ba7512e4178c64609f83428e56d93f5a554c1a
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-05-04 11:33:43 -0300

    feat: add colors and fonts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,12 +16,16 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  ${props => {
+  /* ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `
-  }}
+  }} */
 
+  background-color: ${props => props.theme['green-500']};
   color: ${props => props.theme.white}
 `;
