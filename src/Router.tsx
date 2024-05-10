f8d1bb1eda5ee6515c527c174f46ed50395f7d08
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-05-10 14:15:46 -0300

    feat: set home page styles and structure

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./Layouts/DefaultLayout/DefaultLayout";
 import { History } from "./pages/History";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/Home/Home";
 
 export function Router() {
   return (
