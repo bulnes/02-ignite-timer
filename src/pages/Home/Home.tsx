85163f25a8ec41d6a4d90858b21c05b10fc03a9f
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-05-11 12:52:50 -0300

    feat: set styles to home input fiels

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -18,14 +18,25 @@ export function Home() {
           <TaskInput
             type="text"
             id="task"
+            list="tasks-suggestions"
             placeholder="Dê um nome para seu projeto"
           />
+
+          <datalist id="tasks-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
