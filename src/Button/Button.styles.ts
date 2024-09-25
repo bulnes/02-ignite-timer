fe91562abb87e7d0f535c9bfc6d43824d2916f68
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-09-24 23:54:59 -0300

    feat: install and configure styled components

---
+++ src/Button/Button.styles.ts
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components';
+
+export type ButtonVariant = "primary" | "secondary" | "success" | "danger";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: '#007bff',
+  secondary: '#6c757d',
+  success: '#28a745',
+  danger: '#dc3545'
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 50px;
+
+  ${props => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }}
+`;
