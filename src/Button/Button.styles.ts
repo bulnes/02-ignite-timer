156d77cab3b0a18dbc0a88d47ac32499a5ecf3ea
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-09-25 00:32:21 -0300

    feat: install roboto and roboto mono fonts

--- src/Button/Button.styles.ts
+++ src/Button/Button.styles.ts
@@ -6,21 +6,13 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: '#007bff',
-  secondary: '#6c757d',
-  success: '#28a745',
-  danger: '#dc3545'
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 50px;
 
   ${props => {
     return css`
-      background-color: ${props.theme.primary};
-      background-color: ${buttonVariants[props.variant]};
+      background-color: ${props.theme['green-500']};
     `
   }}
 `;
