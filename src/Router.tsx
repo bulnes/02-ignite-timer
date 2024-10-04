499227fcdca689ef68c8922dab2ee0852d2312da
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-10-04 12:15:30 -0300

    feat: add default layout to the main routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 import { History } from "./pages/History";
 import { Home } from "./pages/Home";
 
@@ -6,11 +7,17 @@ export function Router() {
   const browserRouter = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/history",
-      element: <History />,
+      element: <DefaultLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/history",
+          element: <History />,
+        },
+      ],
     },
   ]);
 
